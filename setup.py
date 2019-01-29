46b4b2dd53b4340eb45706eb68bddac4dac0c633
Stephen Onnen <stephen.onnen@gmail.com>
2019-01-29 12:06:07 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="forum_api",
+    version="0.0.1",
+    author="COM S 309 Team 100",
+    description="A self hosted forum/chat application",
+    liscense="MIT",
+    url="https://git.linux.iastate.edu/CS309SPRING2019/team_100",
+    package="forum_api",
+)
