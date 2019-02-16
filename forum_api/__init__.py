c70fb266e691df2f9630288e3b76ea07682ca994
Stephen Onnen <stephen.onnen@gmail.com>
2019-02-16 11:43:55 -0600

    Fixes to prod dockerfile

--- forum_api/__init__.py
+++ forum_api/__init__.py
@@ -16,9 +16,17 @@ db.init_app(app)
 
 @app.before_first_request
 def init_forum():
+
     db.create_all()
-    hashed = bcrypt.hashpw(FORUM_ADMIN.get("password").encode("utf8"), bcrypt.gensalt())
-    admin = User(
-        username=FORUM_ADMIN.get("username"), pw_hash=hashed, is_admin=True, is_mod=True
-    )
-    admin.save()
+
+    if User.get_user(username=FORUM_ADMIN.get("username")) is None:
+        hashed = bcrypt.hashpw(
+            FORUM_ADMIN.get("password").encode("utf8"), bcrypt.gensalt()
+        )
+        admin = User(
+            username=FORUM_ADMIN.get("username"),
+            pw_hash=hashed,
+            is_admin=True,
+            is_mod=True,
+        )
+        admin.save()
