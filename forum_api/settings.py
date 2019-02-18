8925343294343d65b4db59a1ca4f19a5e1470ec8
Stephen Onnen <stephen.onnen@gmail.com>
2019-02-17 19:02:38 -0600

    Added gitlab-ci.yml

--- forum_api/settings.py
+++ forum_api/settings.py
@@ -4,6 +4,8 @@ from spylogger import get_logger
 
 LOGGER = get_logger(log_level="DEBUG")
 
+FLASK_ENV = os.getenv("FLASK_ENV", "dev")
+
 DEBUG = True
 SECRET_KEY = os.getenv(
     "API_SECRET", "6150645367566B59703373367638792F423F4528482B4D6251655468576D5A71"
@@ -12,7 +14,7 @@ SECRET_KEY = os.getenv(
 SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:{}/{}".format(
     os.getenv("API_DB_USERNAME", "admin"),
     os.getenv("API_DB_PASSWORD", "pass"),
-    os.getenv("API_DB_HOST", "localhost"),
+    os.getenv("API_DB_HOST", "localhost" if FLASK_ENV != "test" else "postgres"),
     os.getenv("API_DB_PORT", "5432"),
     os.getenv("API_DB_NAME", "forum_db"),
 )
